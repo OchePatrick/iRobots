1fa520023419c7323957680eedf973df5ec31eaf
OchePatrick <ogwuchepatrick5@gmail.com>
2021-10-26 22:44:38 +0100

    iRobots

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Oche Patrick',
+        username: 'Oche',
+        email: 'oche@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Torkwase',
+        username: 'Emerald',
+        email: 'emerald@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Benita',
+        username: 'Abeni',
+        email: 'abeni@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Emmanuel Ochai',
+        username: 'Emmz',
+        email: 'emmz@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Apochi Paul',
+        username: 'Apochi',
+        email: 'apochi@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Ene Patience',
+        username: 'Ene',
+        email: 'ene@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Daniel Okoma',
+        username: 'Okoma',
+        email: 'okoma@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Ekele Francis',
+        username: 'Nero',
+        email: 'nero@gmail.com'
+    },
+]
